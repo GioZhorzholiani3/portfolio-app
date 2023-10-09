b0487fdf53f84b4394027a45e22a8182f120b462
GioZhorzholiani3 <giorgi.zhorzholiani.1@btu.edu.ge>
2023-10-09 14:18:16 +0400

    add project section

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -1,7 +1,36 @@
 import classes from "./Portfolio.module.css";
+import projectData from "./ProjectData";
 
 const Portfolio = () => {
-  return <section id="portfolio">Portfolio</section>;
+  return (
+    <section id="portfolio">
+      <h5>My Projects</h5>
+      <h2>Portfolio</h2>
+
+      <div className={`container ${classes.portfolioContainer}`}>
+        {projectData.map((project) => (
+          <article className={classes.portfolioItem} key={project.id}>
+            <div className={classes.portfolioItemImg}>
+              <img src={project.imgSrc} alt="" />
+            </div>
+            <h3>{project.title}</h3>
+            <div className={classes.CTA}>
+              <a href={project.githubLink} className="btn" target="_blank">
+                Github
+              </a>
+              <a
+                href={project.liveDemoLink}
+                className="btn btn-primary"
+                target="_blank"
+              >
+                Live
+              </a>
+            </div>
+          </article>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
---
+++ src/components/portfolio/ProjectData.ts
@@ -0,0 +1,149 @@
+import authenticationApp from "../../assets/project-img/authentication-app.png";
+import diceGame from "../../assets/project-img/dice-game.png";
+import disneyApp from "../../assets/project-img/disney-app.png";
+import expenceApp from "../../assets/project-img/expence-app.png";
+import foodOrderApp from "../../assets/project-img/food-order-app.png";
+import fitnessApp from "../../assets/project-img/fitness-app.png";
+import goalsApp from "../../assets/project-img/goals-app.png";
+import hangmanApp from "../../assets/project-img/hangman-app.png";
+import hotelApp from "../../assets/project-img/hotel-app.png";
+import kanbanApp from "../../assets/project-img/kanban-app.png";
+import memoryGame from "../../assets/project-img/memory-game.png";
+import portfolioApp from "../../assets/project-img/portfolio-app.png";
+import quotesApp from "../../assets/project-img/quotes-app.png";
+import realtorApp from "../../assets/project-img/realtor-app.png";
+import reduxCartApp from "../../assets/project-img/redux-cart-app.png";
+import resumeGeneratorApp from "../../assets/project-img/resume-generator-app.png";
+import shoppingCart from "../../assets/project-img/shopping-cart.png";
+import toursApp from "../../assets/project-img/tours-app.png";
+
+const projectData = [
+  {
+    id: 1,
+    imgSrc: kanbanApp,
+    title: "Kanban App",
+    githubLink: "https://github.com/GioZhorzholiani3/kanban",
+    liveDemoLink: "https://kanban-demo-app.netlify.app/",
+  },
+  {
+    id: 2,
+    imgSrc: memoryGame,
+    title: "Memory Game",
+    githubLink: "https://github.com/GioZhorzholiani3/memory-game",
+    liveDemoLink: "https://lively-beijinho-16064d.netlify.app",
+  },
+  {
+    id: 3,
+    imgSrc: hangmanApp,
+    title: "Hangman",
+    githubLink: "https://github.com/GioZhorzholiani3/hangman",
+    liveDemoLink: "https://hangman-app-gz.netlify.app",
+  },
+  {
+    id: 4,
+    imgSrc: disneyApp,
+    title: "Disney Clone",
+    githubLink: "https://github.com/GioZhorzholiani3/yt-disney-clone",
+    liveDemoLink: "https://gregarious-puppy-edb606.netlify.app",
+  },
+  {
+    id: 5,
+    imgSrc: fitnessApp,
+    title: "Fitness App",
+    githubLink: "https://github.com/GioZhorzholiani3/fitness-app",
+    liveDemoLink: "https://poetic-licorice-1882ca.netlify.app",
+  },
+  {
+    id: 6,
+    imgSrc: quotesApp,
+    title: "Quotes App",
+    githubLink: "https://github.com/GioZhorzholiani3/react-routing",
+    liveDemoLink: "https://fabulous-alpaca-7415b9.netlify.app",
+  },
+  {
+    id: 7,
+    imgSrc: diceGame,
+    title: "Dice Game",
+    githubLink: "https://github.com/GioZhorzholiani3/dice-game",
+    liveDemoLink: "https://nimble-parfait-8d1a28.netlify.app",
+  },
+  {
+    id: 8,
+    imgSrc: shoppingCart,
+    title: "Shopping Cart",
+    githubLink: "https://github.com/GioZhorzholiani3/shopping-cart",
+    liveDemoLink: "https://cozy-khapse-cafcbb.netlify.app",
+  },
+  {
+    id: 9,
+    imgSrc: reduxCartApp,
+    title: "Redux Cart",
+    githubLink: "https://github.com/GioZhorzholiani3/advanced-redux",
+    liveDemoLink: "https://calm-unicorn-5fd74d.netlify.app",
+  },
+  {
+    id: 10,
+    imgSrc: resumeGeneratorApp,
+    title: "CV Generator",
+    githubLink: "https://github.com/GioZhorzholiani3/cv-generator",
+    liveDemoLink: "https://cv-generator-app-gz.netlify.app",
+  },
+  {
+    id: 11,
+    imgSrc: foodOrderApp,
+    title: "Food Order App",
+    githubLink: "https://github.com/GioZhorzholiani3/food-order-app",
+    liveDemoLink: "https://stalwart-sundae-418ed0.netlify.app",
+  },
+  {
+    id: 12,
+    imgSrc: realtorApp,
+    title: "Realtor App",
+    githubLink: "https://github.com/GioZhorzholiani3/House",
+    liveDemoLink: "https://curious-maamoul-76890f.netlify.app",
+  },
+  {
+    id: 13,
+    imgSrc: hotelApp,
+    title: "Hotel App",
+    githubLink: "https://github.com/GioZhorzholiani3/Hotel",
+    liveDemoLink: "https://clinquant-fudge-115fb2.netlify.app",
+  },
+  {
+    id: 14,
+    imgSrc: toursApp,
+    title: "Tours App",
+    githubLink: "https://github.com/GioZhorzholiani3/Natours",
+    liveDemoLink: "https://jade-dolphin-97d7b5.netlify.app",
+  },
+  {
+    id: 15,
+    imgSrc: authenticationApp,
+    title: "Authentication",
+    githubLink: "https://github.com/GioZhorzholiani3/authentication",
+    liveDemoLink: "https://glistening-donut-ad420a.netlify.app",
+  },
+  {
+    id: 16,
+    imgSrc: goalsApp,
+    title: "Goals App",
+    githubLink: "https://github.com/GioZhorzholiani3/goals-app",
+    liveDemoLink: "https://my-goals-app.netlify.app",
+  },
+  {
+    id: 17,
+    imgSrc: expenceApp,
+    title: "Expences App",
+    githubLink: "https://github.com/GioZhorzholiani3/expenses-app",
+    liveDemoLink: "https://incandescent-frangipane-5486bd.netlify.app",
+  },
+  {
+    id: 18,
+    imgSrc: portfolioApp,
+    title: "Portfolio App",
+    githubLink: "https://github.com/GioZhorzholiani3/final",
+    liveDemoLink: "https://stately-puppy-b8ca67.netlify.app",
+  },
+];
+
+export default projectData;
